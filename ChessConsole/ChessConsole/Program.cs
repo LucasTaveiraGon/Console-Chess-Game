ef1e3fcf92c0329e604a601f10849fb11a3cd3c2
Lucas Taveira <lucastaveirag@gmail.com>
2022-01-27 09:57:58 -0300

    Started print pieces in BoardGame1

--- ChessConsole/ChessConsole/Program.cs
+++ ChessConsole/ChessConsole/Program.cs
@@ -8,17 +8,28 @@ namespace ChessConsole
     {
         static void Main(string[] args)
         {
-            BoardGame board = new BoardGame(8, 8);
-
-            
-            board.putPiece(new Tower(board, Collor.Black), new Position(0, 0));
-            board.putPiece(new Tower(board, Collor.Black), new Position(1, 3));
-            board.putPiece(new King(board, Collor.Black), new Position(2, 4));
-          //  board.putPiece(Tower, new Position(0, 0));
-            Screen.printBoard(board);
+            try
+            {
+                BoardGame board = new BoardGame(8, 8);
 
 
+                board.putPiece(new Tower(board, Collor.Black), new Position(0, 0));
+                board.putPiece(new Tower(board, Collor.Black), new Position(1, 3));
+                board.putPiece(new King(board, Collor.Black), new Position(2, 4));
+                //    board.putPiece(Tower, new Position(0, 0));
+                Screen.printBoard(board);
+
+
+
+
+
+            }
+            catch (BoardGameException e) 
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
+
         }
     }
 }
